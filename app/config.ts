076ed8c018cc51cdf03d86080a83ed4d349b41e9
sagar jethi <sagar.jethi007@gmail.com>
2024-03-24 21:58:37 +0530

    env change udpate

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
-export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development'
-    ? 'https://frame-web3-agent.vercel.app/'
-    : 'https://frame-web3-agent.vercel.app/';
+export const NEXT_PUBLIC_URL ="https://frame-web3-agent.vercel.app/";
+  // process.env.NODE_ENV == 'development'
+  //   ? 'https://frame-web3-agent.vercel.app/'
+  //   : 'https://frame-web3-agent.vercel.app/';
